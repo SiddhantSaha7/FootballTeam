f3a171c83960b4e01439c5695c043d6478944d4e
arth2911 <121233489+arth2911@users.noreply.github.com>
2025-03-25 16:14:09 -0400

    Team Interface done.

--- main/Team.java
+++ main/Team.java
@@ -1,4 +1,13 @@
 package main;
 
+import java.util.List;
+
 public interface Team {
+  boolean addPlayer(Player p);
+  void removePlayer(Player p);
+  void assignJerseyNumber();
+  void addPlayersToStartingLineup();
+  List<Player> getPlayer();
+  List<Player> getAllPlayersSorted();
+  List<Player> getStartingLineup();
 }
