1f127e10060bd4c1c396d4e647ad642853afe8a2
Siddhant Saha <siddhantsaha@Siddhants-MacBook-Air.local>
2025-03-31 13:15:06 -0400

    PDP: Added dummy files

---
+++ src/test/BritishColumbiaFootballTeamTest.java
@@ -0,0 +1,31 @@
+
+public class BritishColumbiaFootballTeamTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.Test
+    public void addPlayer() {
+    }
+
+    @org.junit.Test
+    public void removePlayer() {
+    }
+
+    @org.junit.Test
+    public void assignJerseyNumber() {
+    }
+
+    @org.junit.Test
+    public void createStartingLineup() {
+    }
+
+    @org.junit.Test
+    public void getAllPlayersSorted() {
+    }
+
+    @org.junit.Test
+    public void getStartingLineup() {
+    }
+}
---
+++ src/test/SoccerPlayerTest.java
@@ -0,0 +1,63 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SoccerPlayerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getPreferredPosition() {
+    }
+
+    @Test
+    public void setPreferredPosition() {
+    }
+
+    @Test
+    public void getFirstName() {
+    }
+
+    @Test
+    public void setFirstName() {
+    }
+
+    @Test
+    public void getLastName() {
+    }
+
+    @Test
+    public void setLastName() {
+    }
+
+    @Test
+    public void getDOB() {
+    }
+
+    @Test
+    public void getAssignedPosition() {
+    }
+
+    @Test
+    public void setAssignedPosition() {
+    }
+
+    @Test
+    public void getSkillLevel() {
+    }
+
+    @Test
+    public void setSkillLevel() {
+    }
+
+    @Test
+    public void getJerseyNumber() {
+    }
+
+    @Test
+    public void setJerseyNumber() {
+    }
+}
